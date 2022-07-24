c7ae8123eb89f65b97e2dc9756e7818b2606fa49
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-07-24 17:18:30 -0500

    cssUpdateToHome

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -13,6 +13,8 @@ import {
   filterDogsByCreated } from '../../actions'
 import { Link } from 'react-router-dom'
 import DogPic from '../../assets/images/dog_profile.jpg'
+import Bella from '../../assets/images/bella_home.png'
+import './Home.css'
 
 function Home() {
   //const BARK = new Audio(Bark);
@@ -59,11 +61,21 @@ function Home() {
   return(
     <div>
       <NavBar />
-      <h1>WELCOME TO BEFOS</h1>
-      <div className="searchbar-container">
-        <SearchBar />
+      <div className="Header">
+        <div className="left-header">
+          <h1>WELCOME TO BEFOS!</h1>
+          <p> Befos is a website that allows you to find your perfect dog based on the breed information</p>
+          <p> To start you can click on the search icon and then write the name of a dog breed or use<br/> 
+          the filters or pagination(page numbers) below to search in our database</p>
+          <div className="searchbar-container">
+            <SearchBar />
+          </div>
+        </div>
+        <div className="right-header">
+          <img src={Bella} alt="Bella" />
+        </div>
       </div>
-      <div className="pagination">
+      <div className="Pagination">
         <Pagination 
           dogsPerPage={dogsPerPage} 
           allDogs={DOGS.length} 
@@ -71,60 +83,60 @@ function Home() {
       </div>
       <br/>
       <br/>
-      {/* Filtro por orden ascendente / descendente */}
-      <div className="AZZA-Filter">
-        <select onChange={e => {handleOrderByName(e)}}>
-          <option value='az'>A-Z</option>
-          <option value='za'>Z-A</option>
-        </select>
+      <div className="Filters-container">
+        <div className="AZZA-Filter">
+          <select onChange={e => {handleOrderByName(e)}}>
+            <option value='az'>A-Z</option>
+            <option value='za'>Z-A</option>
+          </select>
+        </div>
+        <div className="Temperaments-Filter">
+          <select onChange={e => {handleFilterByTemperament(e)}}>
+            <option value="All">All temperaments...</option>
+            {TEMPERAMENTS.map(temperament => {
+              return(
+                <option 
+                  key={temperament.id} 
+                  value={temperament.name}>
+                  {temperament.name}
+                </option>
+              ) 
+            })}
+          </select>
+        </div>
+        <div className="Created-Filter">
+          <select onChange={e => handleFilterByCreated(e)}>
+            <option value='All'>All dogs</option>
+            <option value='created'>Created in database</option>
+            <option value='api'>Existent (from API)</option>
+          </select>
+        </div>
       </div>
-      <br/>
-      <div className="Temperaments-Filter">
-        <select onChange={e => {handleFilterByTemperament(e)}}>
-          <option value="All">All temperaments...</option>
-          {TEMPERAMENTS.map(temperament => {
+      <div className="Cards-Container">
+        {
+          RENDERED_DOGS.map(dog => {
             return(
-              <option 
-                key={temperament.id} 
-                value={temperament.name}>
-                {temperament.name}
-              </option>
-            ) 
-          })}
-        </select>
+              <Link to={`/dog/${dog.id}`} key={dog.id}>
+                <Card
+                  key={dog.id} 
+                  name={dog.name} 
+                  image={
+                    dog.image?
+                    dog.image.url:
+                    DogPic} 
+                  temperament={
+                    dog.temperament?
+                    dog.temperament.split(', ')[0]:
+                    'No record'} 
+                  weight={
+                    dog.weight.metric?
+                    dog.weight.metric:
+                    'No record'}/>
+              </Link>
+            )
+          })
+        }
       </div>
-      <br/>
-      <div className="Created-Filter">
-        <select onChange={e => handleFilterByCreated(e)}>
-          <option value='All'>All dogs</option>
-          <option value='created'>Created in database</option>
-          <option value='api'>Existent (from API)</option>
-        </select>
-      </div>
-      <br/>
-      {
-        RENDERED_DOGS.map(dog => {
-          return(
-            <Link to={`/dog/${dog.id}`} key={dog.id}>
-              <Card
-                key={dog.id} 
-                name={dog.name} 
-                image={
-                  dog.image?
-                  dog.image.url:
-                  DogPic} 
-                temperament={
-                  dog.temperament?
-                  dog.temperament.split(', ')[0]:
-                  'No record'} 
-                weight={
-                  dog.weight.metric?
-                  dog.weight.metric:
-                  'No record'}/>
-            </Link>
-          )
-        })
-      }
     </div>
   )
 }
--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { searchDogs } from '../../actions'
+import './SearchBar.css'
+import Search from '../../assets/icons/search.png'
 export class SearchBar extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      search: ''
+      search: '',
+      toggle: false
     }
   }
   
@@ -19,24 +22,49 @@ export class SearchBar extends Component {
   handleSubmit(e){
     e.preventDefault();
     this.props.searchDogs(this.state.search);
+    this.setState({
+      search: '',
+      toggle: false
+    })
+  }
+
+  handleToggle(e){
+    e.preventDefault();
+    this.setState({
+      toggle: !this.state.toggle
+    })
   }
 
   render() {
-    const { search } = this.state;
+    const { search, toggle } = this.state;
     return (
-      <div>
-        <input 
-          type="text" 
-          placeholder="Search for a dog breed..." 
-          value={search} 
-          onChange={(e) => this.handleInputChange(e)} 
-        />
-        <button 
-          type= 'submit'
-          onClick={(e) => this.handleSubmit(e)}>
-            Search
-        </button>
-      </div>
+      <>
+      { !toggle ?
+        (
+          <img 
+          src={Search} 
+          alt="search" 
+          className="search-icon"
+          onClick={(e) => this.handleToggle(e)}/>
+        ) :
+        (
+          <div className="box">
+            <input 
+              type="text"  
+              value={search} 
+              onChange={(e) => this.handleInputChange(e)} 
+            />
+            <button 
+              type= 'submit'
+              onClick={(e) => this.handleSubmit(e)}>
+                Search
+            </button>
+          </div>
+        )
+      
+      }
+      </>
+
     )
   }
 }
