6fba4de6c9d2f015e0fce2ee17eaf367bc51e039
thegera4 <84020433+thegera4@users.noreply.github.com>
2022-08-08 10:12:45 -0500

    comments updated

--- api/src/db.js
+++ api/src/db.js
@@ -6,10 +6,11 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
+/* Sustituir todo esto
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+});*/
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
